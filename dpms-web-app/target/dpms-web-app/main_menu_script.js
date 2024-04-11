20939e07fd7e1a380c4429a73d353f15a0ddcabb
Deontae Cocroft <119895365+DeontaeCocroft@users.noreply.github.com>
2024-04-11 12:04:11 -0500

    Delete dpms-web-app directory

--- dpms-web-app/target/dpms-web-app/main_menu_script.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var patientsButton = document.getElementById('patientsButton');
-    patientsButton.addEventListener('click', function() {
-        window.location.href = 'patients.html';
-    });
-});
